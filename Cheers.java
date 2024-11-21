d3d8be43c0cf05f0e78b5ca0298e1659b7b59761
lallyshayah <lally.shayah@post.runi.ac.il>
2024-11-21 13:21:00 +0200

    First solution

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,34 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String str = args[0];
+            int Num = Integer.parseInt(args[1]);
+            String strUpper = "";
+            String anLetters = "AEFHILMNORSX";
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c >= 'a' && c <= 'z') c = (char) (c-32);
+
+                strUpper += c;
+            }
+
+            for (int i = 0; i < strUpper.length(); i++) {
+                char c = strUpper.charAt(i);
+                if (anLetters.indexOf(c) == -1) {
+                        System.out.println("Give me an " + c + ": " + c + "!");
+                }
+
+                else {
+                        System.out.println("Give me a " + c + ": " + c + "!");
+                }
+            }
+
+            System.out.println("What does that spell?");
+
+            for (int i = 0; i < Num; i++) {
+                System.out.println(strUpper + "!!!");
+            }
+
         }
 }
